3249e9ec8674e944e1f2b2713e9c77bf6881900a
patlaf <lafontaine.patrick@gmail.com>
2015-03-18 13:11:14 -0400

    add cluster

--- server.R
+++ server.R
@@ -1,35 +1,37 @@
 library(shiny)
 library(rCharts)
-
+library(plyr)
 #setwd('C:/Users/patlaf/Dropbox/github/DataProduct/')
 
 ## Create a tidier dataset
 house <- read.csv('http://patlaf.com/house.csv')
 house <- house[,-1]
 house <- house[complete.cases(house),]
+house <- house[,-c(5,6)]
 house <- house[house$YearConstruction > 1940,]
+house <- cbind(house,cluster = kmeans(house[,c('longitude', 'latitude')],2,nstart=25)$cluster, row.names = NULL)
 
 
 ## Aggregate the price of the house based on the "Group House" input
 averagePrice <- function(input,house) {
     if(input$groupAggregate == 1) {
-        t <- aggregate(house$Price, by=list(house$NbrBedroom), mean)
-        t$NbrHouse <- as.numeric(table(house$NbrBedroom))
+        t <- aggregate(house$Price, by=list(house$NbrBedroom, house$cluster), mean)
+        t$NbrHouse <- count(house,c('NbrBedroom','cluster'))$freq
         n <- 'Number of Bedrooms'
     } else if (input$groupAggregate == 2) {
-        t <- aggregate(house$Price, by=list(house$YearConstruction), mean)
-        t$NbrHouse <- as.numeric(table(house$YearConstruction))
+        t <- aggregate(house$Price, by=list(house$YearConstruction, house$cluster), mean)
+        t$NbrHouse <- count(house,c('YearConstruction','cluster'))$freq
         n <- 'Year of Construction'
     } else if (input$groupAggregate == 3) {
-        t <- aggregate(house$Price, by=list(house$NbrBathroom), mean)
-        t$NbrHouse <- as.numeric(table(house$NbrBathroom))
+        t <- aggregate(house$Price, by=list(house$NbrBathroom, house$cluster), mean)
+        t$NbrHouse <- count(house,c('NbrBathroom','cluster'))$freq
         n <- 'Number of Bathroom'
     } else {
-        t <- aggregate(house$Price, by=list(house$NbrLevel), mean)
-        t$NbrHouse <- as.numeric(table(house$NbrLevel))
+        t <- aggregate(house$Price, by=list(house$NbrLevel, house$cluster), mean)
+        t$NbrHouse <- count(house,c('NbrLevel','cluster'))$freq
         n <- 'Number of Level'
     }
-    names(t) <- c(n,'Mean Price','Number of Houses')
+    names(t) <- c(n,'Cluster' ,'Mean Price','Number of Houses')
     t
 }
 
@@ -46,9 +48,15 @@ datasetInfo <- function(house) {
 subsetHouse <- function(input,house) {
     house <- subset(house,NbrBedroom <= input$bedroom)
     house <- subset(house,(YearConstruction >= as.numeric(input$year[1]) & YearConstruction <= as.numeric(input$year[2])))
-    house                
+    splitCluster(input$NbrCluster,house)                
 }
 
+
+## create cluster
+splitCluster <- function(k, house) {
+    house$cluster = kmeans(house[,c('longitude', 'latitude')],k,nstart=25)$cluster
+    house
+}
 
 ## Output the value
 shinyServer(function(input, output) {
@@ -60,8 +68,8 @@ shinyServer(function(input, output) {
     
     output$graph <- renderChart2({
         t2 <- averagePrice(input, subsetHouse(input,house))
-        names(t2) <- c('Group','Price')
-        x1 <- nPlot(Price~Group, type="lineChart", data=t2)
+        names(t2) <- c('Group','Cluster','Price')
+        x1 <- nPlot(Price~Group, type="lineChart", group= 'Cluster', data=t2)
         x1$yAxis(axisLabel = 'Price', tickFormat = "#! function(d) {return '$' + d} !#")
         return(x1)
     })
--- ui.R
+++ ui.R
@@ -16,35 +16,36 @@ shinyUI(pageWithSidebar(
     
     sidebarPanel(
         p('Find the code at http://github.com/patlaf/DataProduct'),
-        h4('Initial house dataset summary:'),
-        verbatimTextOutput('datasetInfo'),
+
+        h3('Group Houses'),
+        selectInput("groupAggregate", label = 'Group Houses by ', 
+                    choices = list("Number of Bedroom" = 1, "Number of Bathroom" = 3, "Number of Level" = 4,"Year of Construction" = 2),selected = 1),
+        
+        h3('Number of cluster'),
+        numericInput('NbrCluster',label="How many clusters",value=2),
         
         hr(),
         
         h3('Filter Dataset'),
         p('Use the slider to filter the number of houses'),
-        sliderInput("year","Construction Years ", min=min(house$YearConstruction), max=max(house$YearConstruction), value=c(1990,2014)),
-        sliderInput("bedroom","Maximum Number of Bedrooms:",min = min(house$NbrBedroom),max = max(house$NbrBedroom),value = 3),
-        
-        h3('Group Houses'),
-        p('Select how the price will by aggregated'),
-        selectInput("groupAggregate", label = 'Group Houses by ', 
-                    choices = list("Number of Bedroom" = 1, "Number of Bathroom" = 3, "Number of Level" = 4,"Year of Construction" = 2),selected = 1),
+        sliderInput("year","Construction Years ", min=min(house$YearConstruction), max=max(house$YearConstruction), value=c(1990,2014), step=1),
+        sliderInput("bedroom","Maximum Number of Bedrooms:",min = min(house$NbrBedroom),max = max(house$NbrBedroom),value = 3,step=1),
+
         hr(),
-        
+        h4('Initial house dataset summary:'),
+        verbatimTextOutput('datasetInfo'),
         h4('Base on selection, the house dataset is now:'),
         verbatimTextOutput('filteredDatasetInfo')
     ),
         
-    mainPanel(
-        p('An application to show the mean price of houses depending on the number of bedrooms, bathrooms, levels or year of construction'),
-        
+    mainPanel(        
         h3('Mean Price'),
-        h5('Base on the filter on the left (in the menu): '),
-        htmlOutput('Price'),
+        p('The application shows the mean price of houses depending on the number of bedrooms, bathrooms, levels or year of construction on different neighborhoods.'),
+        p('The clusters are created base on latitude/longitude to represent the neighborhoods.'),
         showOutput('graph','nvd3'),
-        
-        
+        htmlOutput('Price'),
+
+
         h3('Filtered Dataset'),
         dataTableOutput('dataset')
     )
